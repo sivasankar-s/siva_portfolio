75d2178ef0cadbcc75dda5acba4d6560d093aee8
sivasankar-s <siva.sankar.s@outlook.com>
2024-01-17 22:53:43 +0530

    home fix

--- src/router.jsx
+++ src/router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "./home";
+import Home from "./Home";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
