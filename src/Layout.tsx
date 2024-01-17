8e966f9ce9486b5d1a728d44c76c4ac6cd236750
sivasankar-s <siva.sankar.s@outlook.com>
2024-01-17 23:02:16 +0530

    dep fix2

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from './components/NavBar'
+import Navbar from './components/Navbar'
 
 const Layout = () => {
   return (
